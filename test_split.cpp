2281bc675e56f5000cdcbf9be8995c3339495f09
Jesse Tzo <tzo@usc.edu>
2024-01-25 22:21:26 -0800

    test files

--- test_split.cpp
+++ test_split.cpp
@@ -10,8 +10,64 @@ g++ split.cpp test_split.cpp -o test_split
 */
 
 #include "split.h"
+#include <iostream> 
+using namespace std;
 
 int main(int argc, char* argv[])
 {
+   Node* head = new Node(1, nullptr);
+   Node* current = new Node(3, nullptr);
+   head -> next = current;
+   Node* prev = current;
+   current = new Node(5, nullptr);
+   prev -> next = current;
+   prev = current;
+   current = new Node(6, nullptr);
+   prev -> next = current;
+
+   // Node* head = new Node(0, nullptr);
+   // Node* current = new Node(20, nullptr);
+   // head -> next = current;
+
+   Node* odds = nullptr;
+   Node* evens = nullptr;
+
+   // print list!
+   cout << "Original: ";
+   for (Node* iter = head; iter != nullptr; iter = iter -> next) {
+      if (iter -> next == nullptr) {
+         cout << iter -> value;
+      }
+      else  {
+         cout << iter -> value << "->";
+      }
+   }  
+
+   split(head, odds, evens);
+
+   cout << endl << "Odds: ";
+   // print list!
+   for (Node* iter = odds; iter != nullptr; iter = iter -> next) {
+      if (iter -> next == nullptr) {
+         cout << iter -> value;
+      }
+      else  {
+         cout << iter -> value << "->";
+      }
+   } 
+
+   cout << endl;
+
+   cout << "Evens: ";
+   for (Node* iter = evens; iter != nullptr; iter = iter -> next) {
+      if (iter -> next == nullptr) {
+         cout << iter -> value;
+      }
+      else  {
+         cout << iter -> value << "->";
+      }
+   } 
+
+
 
 }
