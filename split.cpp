6fa09171a1828de2082948531887d3b49cb4379c
Jesse Tzo <tzo@usc.edu>
2024-01-25 17:43:04 -0800

    all done with part 1

--- split.cpp
+++ split.cpp
@@ -2,9 +2,9 @@
 CSCI 104: Homework 1 Problem 1
 
 Write a recursive function to split a sorted singly-linked
-list into two sorted linked lists, where one has the even 
-numbers and the other contains the odd numbers. Students 
-will receive no credit for non-recursive solutions. 
+list into two sorted linked lists, where one has the even
+numbers and the other contains the odd numbers. Students
+will receive no credit for non-recursive solutions.
 To test your program write a separate .cpp file and #include
 split.h.  **Do NOT add main() to this file**.  When you submit
 the function below should be the only one in this file.
@@ -14,10 +14,32 @@ the function below should be the only one in this file.
 
 /* Add a prototype for a helper function here if you need */
 
-void split(Node*& in, Node*& odds, Node*& evens)
+void split(Node *&in, Node *&odds, Node *&evens)
 {
-  /* Add code here */
-// WRITE YOUR CODE HERE
+  if (in != nullptr)
+  {
+    if (in->value % 2 == 1)
+    {
+      odds = in;
+      in = in->next;
+      odds->next = nullptr;
+      split(in, odds->next, evens);
+    }
+    else
+    {
+      evens = in;
+      in = in->next;
+      evens->next = nullptr;
+      split(in, odds, evens->next);
+    }
+  }
+
+  else
+  {
+    in = nullptr;
+    odds = nullptr;
+    evens = nullptr;
+  }
 }
 
 /* If you needed a helper function, write it here */
